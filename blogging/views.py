d92465e60517f7b4b547e20d587af00f5d7858b6
Lauren Parker <laurparker1990@gmail.com>
2022-04-26 22:00:39 -0700

    Saving work. About to begin Static Files

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.http import HttpResponse, Http404
 from blogging.models import Post
+from django.template import loader
 
 
 def stub_view(request, *args, **kwargs):
@@ -18,5 +18,18 @@ def stub_view(request, *args, **kwargs):
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
+    # template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
+    # body = template.render(context)
+    # return HttpResponse(body, content_type="text/html")
     return render(request, 'list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
